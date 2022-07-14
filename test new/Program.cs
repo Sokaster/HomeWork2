1b4dfe6c58dcc49464fc685d46413497a5ab3217
unknown <danenkov.dmitriy@list.ru>
2022-07-14 12:54:32 +0300

    hometest

---
+++ test new/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press number of factorial, which you need");
+            int x = int.Parse(Console.ReadLine());
+            int n = x;
+            if (x > 0)
+            {
+               
+                          for (int i = n - 1; i > 0; i--)
+                           {
+                              n *= i;
+                           }
+                            Console.WriteLine($"factorial chisla {x} ! = {n}");
+             }
+             if (x == 0)
+            {
+                Console.WriteLine($"factorial {x} ! = 1");
+            }
+             if (x < 0)
+            {
+                Console.Write("VVedite polozhitelnoe chislo");
+            } 
+                
+                
+        }
+
+                
+                
+            
+            
+           
+        }
+    }
